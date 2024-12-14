113a7b6d09113175aa5a75b499710ac4f7596d98
zander80085 <zander.norris@uconn.edu>
2024-12-14 16:00:22 -0500

    new

--- fetchAudio.py
+++ fetchAudio.py
@@ -1,5 +1,5 @@
+import os, io
 from gtts import gTTS
-import os
 from pydub import AudioSegment
 from pydub.utils import which
 from moviepy.editor import AudioFileClip
@@ -7,29 +7,27 @@ from moviepy.editor import AudioFileClip
 AudioSegment.converter = which("ffmpeg")
 
 def text_to_speech(comments):
-    if not os.path.exists('AudioFiles'):
-        os.makedirs('AudioFiles')
-
-    audio_files = []
+    if not os.path.exists('Audio'): os.makedirs('Audio')
+    comment_path = 'Audio'
     for i, comment in enumerate(comments):
         tts = gTTS(text=comment, lang='en')
-        file_path = os.path.join('AudioFiles', f'comment_{i}.mp3')
-        tts.save(file_path)
-        audio_files.append(file_path)
-        print(f'Successfully saved comment_{i}.mp3...')
-    return audio_files
+        tts.save(f'{comment_path}/comment_{i}.mp3')
+        if i == 0: print(f'Successfully saved comment_{i}.mp3 (title)')
+        else: print(f'Successfully saved comment_{i}.mp3')
+    mp3_files = os.listdir(comment_path)
+    return mp3_files, comment_path
 
-def combine_audio_files(audio_files):
-    if not os.path.exists('AggregatedAudio'):
-        os.makedirs('AggregatedAudio')
-    output_path = os.path.join('AggregatedAudio', 'combined_audio.mp3')
-    combined = AudioSegment.empty()
-    for file_path in audio_files:
-        audio = AudioSegment.from_mp3(file_path)
-        combined += audio
-    combined.export(output_path, format='mp3')
-    print('Successfully combined audio files...')
-    return output_path
+def combine_mp3(mp3_files, comment_path):
+    if not os.path.exists('AllAudio'): os.makedirs('AllAudio')
+    final_output_path = os.path.join('AllAudio', 'combined_audio.mp3')
+    mp3_path = os.path.join(comment_path , mp3_files[0])
+    print(mp3_path)
+    combined_mp3 = AudioSegment.from_mp3(mp3_path)
+    for file in mp3_files[1:]:
+        next_file = AudioSegment.from_mp3(file)
+        combined_mp3 += next_file
+    combined_mp3.export(final_output_path, format = 'mp3')
+    return final_output_path
 
 def audio_length(audio_path):
     audio_clip = AudioFileClip(audio_path)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from createVideo import clip_video, combine_video_audio_picture, crop_to_vertical
 from fetchComments import fetch_comment_text
-from fetchAudio import text_to_speech, combine_audio_files, audio_length
+from fetchAudio import text_to_speech, combine_mp3, audio_length
 from fetchVideo import download_youtube_video
 from titleImage import capture_title_image
 from cleanup import delete_specific_folders_in_current_directory
@@ -11,24 +11,28 @@ if __name__ == '__main__':
     print('Welcome to generative short video creation!')
     print('-'*50)
     
-    reddit_url = input('What is the link of the reddit thread? ')
-    youtube_url = input('What is the link for the youtube video? ')
+    # reddit_url = input('What is the link of the reddit thread? ')
+    # youtube_url = input('What is the link for the youtube video? ')
+
+    reddit_url = 'https://www.reddit.com/r/AskReddit/comments/1hdueny/what_are_the_signs_youve_noticed_that_youre/'
+    youtube_url = 'https://www.youtube.com/watch?v=u7kdVe8q5zs&t=13s'
 
     title_path = capture_title_image(reddit_url)
     comments = fetch_comment_text(reddit_url)
 
-    comments_audio = text_to_speech(comments)
-    aggregated_comments_audio = combine_audio_files(comments_audio)
+    mp3_files, comment_audio_path = text_to_speech(comments)
+
+    combined_audio = combine_mp3(mp3_files, comment_audio_path)
 
     video_path = download_youtube_video(youtube_url)
     start_time = 45
-    end_time = audio_length(aggregated_comments_audio) + start_time
+    end_time = audio_length(combined_audio) + start_time
 
     clipped_video_path = clip_video(video_path, start_time, end_time)
 
     cropped_video_path = crop_to_vertical(clipped_video_path)
 
-    output_path = combine_video_audio_picture(cropped_video_path, aggregated_comments_audio, title_path)
+    output_path = combine_video_audio_picture(cropped_video_path, combined_audio, title_path)
     # create subtitles
     transcription(output_path)
     # delete_specific_folders_in_current_directory(temp_dir)
